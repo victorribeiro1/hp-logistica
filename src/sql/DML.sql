513ad6994109fa59c4073896aa5659990a98eeee
victorribeiro1 <victorribeiro2929@gmail.com>
2022-02-02 00:47:42 -0300

    update contact scss

--- src/sql/DML.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO avaliacoes (avaliador, avaliacao) VALUES
-   ("Thelminha da Silva", "Fui realmente muito bom atendida. Nota 1000. Fui realmente muito bom atendida. Nota 1000. Fui realmente muito bom atendida. Nota 1000."),
-   ("Marcao de Jesus", ". O melhor atendimento da minha vida. Recomendo esta empresa. O melhor atendimento da minha vida. Recomendo esta empresa."),
-   ("Geremias Dantas", "Se você está procurando uma empresa para prestar serviços logísticos para você, está no lugar certo!"),
-   ("Janaína Cruz", "Muito atenciosos e prestativos. Muito atenciosos e prestativos. Muito atenciosos e prestativos."),
-   ("Kile Afonso", "Empresa excelênte! Recomendo fortemente. Empresa excelênte! Recomendo fortemente. Empresa excelênte! Recomendo fortemente.")
-
-SELECT * FROM avaliacoes;
