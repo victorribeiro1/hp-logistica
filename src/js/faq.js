3eba6179bb1705c47fb31b0bb5e6d435c0f194e0
victorribeiro1 <victorribeiro2929@gmail.com>
2022-03-02 00:51:22 -0300

    update home (faq--mobile)

--- src/js/faq.js
+++ src/js/faq.js
@@ -5,16 +5,16 @@ faqs.forEach(faq => {
    const lineBox = faq.querySelector('.faq__options__option__line-box')
    const answerBox = faq.querySelector('.faq__options__option__answer-box')
 
-   
+
    faq.style.height = questionBox.clientHeight + (lineBox.clientHeight / 2) + 'px'
 
-   
+
    faq.addEventListener('mouseenter', () => {
       const questionBox = faq.querySelector('.faq__options__option__question-box')
       const lineBox = faq.querySelector('.faq__options__option__line-box')
       const answerBox = faq.querySelector('.faq__options__option__answer-box')
 
-      
+
 
       faq.style.height = questionBox.clientHeight + (lineBox.clientHeight / 2) + lineBox.clientHeight + 70 + answerBox.clientHeight +  'px'
       // console.log(faq.querySelector(questionBox).clientHeight)
@@ -27,15 +27,57 @@ faqs.forEach(faq => {
       const lineBox = faq.querySelector('.faq__options__option__line-box')
       const answerBox = faq.querySelector('.faq__options__option__answer-box')
 
-      
+
       faq.style.height = questionBox.clientHeight + (lineBox.clientHeight / 2) + lineBox.clientHeight + 70 + answerBox.clientHeight +  'px'
       // console.log(faq.querySelector(questionBox).clientHeight)
       // console.log(faq.querySelector(answerBox).clientHeight)
 
    })
-   
+
    faq.addEventListener('mouseout', () => {
       faq.style.height = questionBox.clientHeight + (lineBox.clientHeight / 2) + 'px'
 
+   })
+})
+
+window.addEventListener('resize', () => {
+   faqs.forEach(faq => {
+      const questionBox = faq.querySelector('.faq__options__option__question-box')
+      const lineBox = faq.querySelector('.faq__options__option__line-box')
+      const answerBox = faq.querySelector('.faq__options__option__answer-box')
+
+
+      faq.style.height = questionBox.clientHeight + (lineBox.clientHeight / 2) + 'px'
+
+
+      faq.addEventListener('mouseenter', () => {
+         const questionBox = faq.querySelector('.faq__options__option__question-box')
+         const lineBox = faq.querySelector('.faq__options__option__line-box')
+         const answerBox = faq.querySelector('.faq__options__option__answer-box')
+
+
+
+         faq.style.height = questionBox.clientHeight + (lineBox.clientHeight / 2) + lineBox.clientHeight + 70 + answerBox.clientHeight +  'px'
+         // console.log(faq.querySelector(questionBox).clientHeight)
+         // console.log(faq.querySelector(answerBox).clientHeight)
+
+      })
+
+      faq.addEventListener('mousemove', () => {
+         const questionBox = faq.querySelector('.faq__options__option__question-box')
+         const lineBox = faq.querySelector('.faq__options__option__line-box')
+         const answerBox = faq.querySelector('.faq__options__option__answer-box')
+
+
+         faq.style.height = questionBox.clientHeight + (lineBox.clientHeight / 2) + lineBox.clientHeight + 70 + answerBox.clientHeight +  'px'
+         // console.log(faq.querySelector(questionBox).clientHeight)
+         // console.log(faq.querySelector(answerBox).clientHeight)
+
+      })
+
+      faq.addEventListener('mouseout', () => {
+         faq.style.height = questionBox.clientHeight + (lineBox.clientHeight / 2) + 'px'
+
+      })
    })
 })
