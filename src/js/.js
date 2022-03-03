8ddc85e6a4ea33c8ba26214b6f077049843f0eed
victorribeiro1 <victorribeiro2929@gmail.com>
2022-03-03 13:30:49 -0300

    udpate script da area de contato

---
+++ src/js/.js
@@ -0,0 +1,1 @@
+console.log('victorribeiro2929'.split('@gmail.com'))
--- src/js/contato.js
+++ src/js/contato.js
@@ -1,36 +1,120 @@
 const form = document.getElementById('form')
 const reviewButton = document.getElementById('reviewButton')
-const editButton = document.getElementById('editButton')
-const sendButton = document.getElementById('sendButton')
+// const editButton = document.getElementById('editButton')
+// const sendButton = document.getElementById('sendButton')
 const inputs = document.querySelectorAll('.input')
 
-console.log(form, reviewButton, editButton, sendButton, inputs)
-
-class EmailManagement {
-   constructor(form, sendButton, inputs) {
-      this.form = form
-      this.sendButton = sendButton
+class Validation {
+   constructor(inputs) {
       this.inputs = inputs
    }
 
-   validate() {
+   isBlank() {
+      let f_isBlank = false // 'f' de 'flag'
+
       this.inputs.forEach(input => {
-         console.log('yeap')
+         if (input.value == '') f_isBlank = true
       })
+
+      return f_isBlank
+   }
+
+   // markInputs() {
+
+   // }
+
+   checkEmail() {
+      let f_checkEmail = false
+
+      const email = this.inputs[0].value
+      const emailArray = email.split('@gmail.com')
+
+      if (emailArray.length === 2) {
+         f_checkEmail = true
+         this.inputs[0].classList.remove('incorrect')
+      } else {
+         this.inputs[0].classList.add('incorrect')
+      }
+
+      return f_checkEmail
+   }
+
+   checkLength() {
+      let f_checkLength = true
+
+      if (this.inputs[0].value.length < 15)  {
+         this.inputs[0].classList.add('incorrect')
+         f_checkLength  =  false
+      } else {
+         this.inputs[0].classList.remove('incorrect')
+      }
+
+      if (this.inputs[1].value.length < 10)  {
+         this.inputs[1].classList.add('incorrect')
+         f_checkLength  =  false
+      } else {
+         this.inputs[1].classList.remove('incorrect')
+      }
+
+      if (this.inputs[2].value.length < 4)   {
+         this.inputs[2].classList.add('incorrect')
+         f_checkLength  =  false
+      } else {
+         this.inputs[2].classList.remove('incorrect')
+      }
+
+      if (this.inputs[3].value.length < 4)   {
+         this.inputs[3].classList.add('incorrect')
+         f_checkLength  =  false
+      } else {
+         this.inputs[3].classList.remove('incorrect')
+      }
+
+      if (this.inputs[4].value.length < 25)  {
+         this.inputs[4].classList.add('incorrect')
+         f_checkLength  =  false
+      } else {
+         this.inputs[4].classList.remove('incorrect')
+      }
+
+
+      return f_checkLength
+   }
+}
+
+class EmailManagement {
+   constructor(form, reviewButton, inputs) {
+      this.form = form
+      this.reviewButton = reviewButton
+      this.inputs = inputs
+   }
+
+   isValid() {
+      let validation = new Validation(this.inputs)
+
+      let isValid = true
+
+      validation.isBlank()
+      validation.checkLength()
+      validation.checkEmail()
+
+      if (validation.isBlank() || !validation.checkEmail() || !validation.checkLength()) {
+         isValid = false
+      }
+
+      return isValid
    }
 
    listenUp() {
-      alert('ok')
       this.reviewButton.addEventListener('click', () => {
          this.form.addEventListener('submit', event => event.preventDefault())
-         // this.validate()
+
+         if(this.isValid()) {
+
+         }
       })
    }
 }
 
-const emailManager = new EmailManagement(form, sendButton, inputs)
-// emailManager.listenUp()
-
-// console.log
-
-// criar classe 'Validator', responsável pela validação dos inputs
+const emailManager = new EmailManagement(form, reviewButton, inputs)
+emailManager.listenUp()
