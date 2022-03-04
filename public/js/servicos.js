ed6bb229ac23dd5b44f6beffd9b08b144aef930a
victorribeiro1 <victorribeiro2929@gmail.com>
2022-03-04 13:50:24 -0300

    update general

---
+++ public/js/servicos.js
@@ -0,0 +1,28 @@
+const serviceCards = document.querySelectorAll('.serviceCard')
+const saibaMais = document.querySelector('.saiba-mais__box')
+
+serviceCards.forEach(serviceCard => {
+   serviceCard.addEventListener('click', () => {
+      serviceCard.classList.toggle('active')
+   })
+})
+
+const saibaMaisButtons = document.querySelectorAll('.saiba-mais-button')
+saibaMaisButtons.forEach(saibaMaisButton => {
+   saibaMaisButton.addEventListener('click', () => {
+      const service = saibaMaisButton.getAttribute('service')
+      console.log(saibaMaisButton.getAttribute('service'))
+      saibaMais.classList.add('active')
+      console.log(document.querySelector(`.popUp[service=${service}]`))
+      document.querySelector(`.popUp[service=${service}]`).classList.add('active')
+
+   })
+})
+
+const xs = document.querySelectorAll('#x')
+xs.forEach(x => {
+   x.addEventListener('click', () => {
+      document.querySelector('.popUp.active').classList.remove('active')
+      saibaMais.classList.remove('active')
+   })
+})
