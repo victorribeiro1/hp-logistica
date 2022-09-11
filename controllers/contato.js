883b2b981eae0e24ae06c8636179dade347ce590
victorribeiro1 <victorribeiro2929@gmail.com>
2022-09-11 00:34:05 -0300

    Update

--- controllers/contato.js
+++
@@ -1,5 +0,0 @@
-const path = require('path')
-
-exports.contactPage = (req, res) => {
-   res.sendFile(path.resolve(__dirname, '../', 'public', 'contato.html'))
-}
--- controllers/servicos.js
+++
@@ -1,5 +0,0 @@
-const path = require('path')
-
-exports.ServicesPage = (req, res) => {
-   res.sendFile(path.resolve(__dirname, '../', 'public', 'servicos.html'))
-}
