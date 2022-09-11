ffa5c8f11c63b4a4c8565458c4b89e5e29118d64
victorribeiro1 <victorribeiro2929@gmail.com>
2022-09-11 00:28:22 -0300

    Update

--- public/js/contato.js
+++ public/js/contato.js
@@ -5,6 +5,9 @@ const reviewBox = document.getElementById('reviewBox')
 const editButton = document.getElementById('editButton')
 const sendButton = document.getElementById('sendButton')
 
+let text
+let from
+
 class Validation {
    constructor(inputs) {
       this.inputs = inputs
@@ -139,8 +142,49 @@ class EmailManagement {
 
       blur.addEventListener('click', () => this.closeReviewBox())
       this.editButton.addEventListener('click', () => this.closeReviewBox())
+
+      this.sendButton.addEventListener('click', () => {
+         if (localStorage.getItem('jaEnviou') == 'true') {
+            console.log('email não enviado')
+            return
+         } else {
+            console.log('email enviado')
+            localStorage.setItem('jaEnviou', 'true')
+            this.closeReviewBox()
+            from = inputs[0]
+            text = `Email: ${inputs[0]}< br />Nome: ${inputs[1]}< br />Cargo:${inputs[2]}< br />Empresa:${inputs[3]}< br />Mensagem:${inputs[4]}`
+            run()
+         }
+      })
    }
 }
 
 const emailManager = new EmailManagement(form, reviewButton, inputs, reviewBox, editButton, sendButton)
 emailManager.listenUp()
+
+import nodemailer from ('../node_modules/nodemailer')
+const transporter = nodemailer.createTransport({
+   host: 'br556.hostgator.com.br',
+   port: 587,
+   secure: false,
+   auth: {
+      user: 'contato@hplogistica.com',
+      pass: 'Hp17042611*',
+   },
+   tls: {
+      rejectUnauthorized: false
+   }
+})
+
+async function run() {
+   const mailSent = await transporter.sendMail({
+      from,
+      subject: 'Email enviado através do site',
+      text,
+      to: 'contato@hplogistica.com'
+   })
+
+   console.log(mailSent)
+}
+
+// const form=document.getElementById("form"),reviewButton=document.getElementById("reviewButton"),inputs=document.querySelectorAll(".input"),reviewBox=document.getElementById("reviewBox"),editButton=document.getElementById("editButton"),sendButton=document.getElementById("sendButton");let text,from;class Validation{constructor(a){this.inputs=a}isBlank(){let a=!1;return this.inputs.forEach(b=>{""==b.value&&(a=!0)}),a}checkEmail(){let a=!1,b=this.inputs[0].value,c=b.split("@gmail.com");return 2===c.length?(a=!0,this.inputs[0].classList.remove("incorrect")):this.inputs[0].classList.add("incorrect"),a}checkLength(){let a=!0;return this.inputs[0].value.length<15?(this.inputs[0].classList.add("incorrect"),a=!1):this.inputs[0].classList.remove("incorrect"),this.inputs[1].value.length<10?(this.inputs[1].classList.add("incorrect"),a=!1):this.inputs[1].classList.remove("incorrect"),this.inputs[2].value.length<4?(this.inputs[2].classList.add("incorrect"),a=!1):this.inputs[2].classList.remove("incorrect"),this.inputs[3].value.length<4?(this.inputs[3].classList.add("incorrect"),a=!1):this.inputs[3].classList.remove("incorrect"),this.inputs[4].value.length<25?(this.inputs[4].classList.add("incorrect"),a=!1):this.inputs[4].classList.remove("incorrect"),a}}class EmailManagement{constructor(a,b,c,d,e,f){this.form=a,this.reviewButton=b,this.inputs=c,this.reviewBox=d,this.editButton=e,this.sendButton=f}isValid(){let a=new Validation(this.inputs),b=!0;return a.isBlank(),a.checkLength(),a.checkEmail(),!a.isBlank()&&a.checkEmail()&&a.checkLength()||(b=!1),b}getAndSetReviewMessage(){let a=this.inputs[4].value,b=document.getElementById("messageDestination");b.innerText=a}activeReviewBox(){this.reviewBox.classList.add("active")}closeReviewBox(){this.reviewBox.classList.remove("active")}listenUp(){this.reviewButton.addEventListener("click",()=>{this.form.addEventListener("submit",a=>a.preventDefault()),this.isValid()&&(this.getAndSetReviewMessage(),this.activeReviewBox())});let a=document.querySelector(".contact__message__blur");a.addEventListener("click",()=>this.closeReviewBox()),this.editButton.addEventListener("click",()=>this.closeReviewBox()),this.sendButton.addEventListener("click",()=>{if("true"==localStorage.getItem("jaEnviou")){console.log("email n\xe3o enviado");return}console.log("email enviado"),localStorage.setItem("jaEnviou","true"),this.closeReviewBox(),from=inputs[0],text=`Email: ${inputs[0]}< br />Nome: ${inputs[1]}< br />Cargo:${inputs[2]}< br />Empresa:${inputs[3]}< br />Mensagem:${inputs[4]}`,run()})}}const emailManager=new EmailManagement(form,reviewButton,inputs,reviewBox,editButton,sendButton);emailManager.listenUp();const nodemailer=require("./nodemailer.js");console.log(nodemailer);const transporter=nodemailer.createTransport({host:"br556.hostgator.com.br",port:587,secure:!1,auth:{user:"contato@hplogistica.com",pass:"Hp17042611*"},tls:{rejectUnauthorized:!1}});async function run(){let a=await transporter.sendMail({from,subject:"Email enviado atrav\xe9s do site",text,to:"contato@hplogistica.com"});console.log(a)}
--- public/js/horizontal-slider.js
+++ public/js/horizontal-slider.js
@@ -15,15 +15,17 @@ let distFromTop = document.querySelector(".horizontal-section").offsetTop;
 
 let scrollDistance = distFromTop + horLength - windowWidth;
 
-document.querySelector(".horizontal-section").style.height = horLength + "px";
+document.querySelector(".horizontal-section").style.height = horLength + 1300 + "px";
 
 
 window.onscroll = function(){
-  let scrollTop = window.pageYOffset;
+   let scrollTop = window.pageYOffset;
 
-  if (scrollTop >= distFromTop && scrollTop <= scrollDistance) {
+   if (scrollTop >= distFromTop && scrollTop <= scrollDistance) {
     document.querySelector(".element-wrapper").style.transform = "translateX(-"+(scrollTop - distFromTop)+"px)";
-  }
+   }
+
+   console.log(scrollTop)
 }
 
 window.onresize = function() {
@@ -33,4 +35,5 @@ window.onresize = function() {
    distFromTop = document.querySelector(".horizontal-section").offsetTop;
 
    scrollDistance = distFromTop + horLength - windowWidth;
+   document.querySelector(".horizontal-section").style.height = horLength + "px";
 }
